2515429d742fdcf2a0638db3a7a6e794183470f2
EC2 Default User <ec2-user@ip-172-31-48-179.us-west-2.compute.internal>
2019-11-20 00:12:31 +0000

    faker

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+const faker = require("faker");
 const express = require("express");
 const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"))
 //routes
 app.get("/", function(req, res) {
-   res.render("index.html");
+   res.render("index.html", { "fakeMultiParagraphData": faker.lorem.paragraphs() });
 });
 
 app.get("/windows", function(req, res) {
-   res.render("windows.html");
+   res.render("windows.html", { "fakeParagraphData": faker.lorem.paragraph() });
 });
 
 app.get("/mac", function(req, res) {
-   res.render("mac.html");
+   res.render("mac.html", { "fakeParagraphData": faker.lorem.paragraph() });
 });
 
 app.get("/linux", function(req, res) {
-   res.render("linux.html");
+   res.render("linux.html", { "fakeParagraphData": faker.lorem.paragraph() });
 });
 
 //server listener
